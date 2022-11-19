b269f39c3420948fad8e0a55c829b07ba154b70d
Richard <55552821+Em1tt@users.noreply.github.com>
2022-11-19 01:35:16 +0100

    Update gen.js

--- gen.js
+++ gen.js
@@ -1,5 +1,6 @@
 class CSSKeyframeGen {
-    constructor(radius, steps,  offsetX, offsetY, rotation){
+    constructor(name, radius, steps,  offsetX, offsetY, rotation){
+        this.name = name;
         this.radius = radius;
         this.steps = steps;
         this.offsetX = offsetX;
@@ -15,7 +16,7 @@ class CSSKeyframeGen {
             const b = Math.sqrt(Math.pow(a,2)-Math.pow(height,2));
             this.coordinates.push([(b+offsetX),(( steps/2 > i?1:-1) * height+offsetY)]); //Detect half of animation, invert Y value so the animation doesn't loop back the same way around
         };
-        this.kf = `@keyframes circle{${this.coordinates.map((v,i) => {
+        this.kf = `@keyframes ${name}{${this.coordinates.map((v,i) => {
             return `${Math.floor(100/(this.coordinates.length-1)*i)}%{transform: translate(${v[0]}px, ${v[1]}px);}`;
         }).join("")}}`;
     }
