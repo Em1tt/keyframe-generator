876ad9dc5d3d73f719708d06f6b2392fc72db64a
Richard <55552821+Em1tt@users.noreply.github.com>
2022-11-19 01:01:29 +0100

    Update gen.js

--- gen.js
+++ gen.js
@@ -20,41 +20,19 @@ for (let i = 0; i < 7; i++) {
     }
 }
 
-for (let i = 1; i < 13; i++) {
-    const alphaRad = 2*Math.PI/steps*i;
-    console.log(alphaRad);
-    const a = Math.sqrt(2 - 2 * Math.cos(alphaRad))*radius*-rotation;
-    const height = a/radius * Math.sqrt(Math.pow(radius,2) - Math.pow(a,2)/4);
-    
-    const b = Math.sqrt(Math.pow(a,2)-Math.pow(height,2));
-    if(steps / 2 > i){
-        coordinates.push([(b+0),(height+offsetY)]);
-    }else{
-        coordinates.push([(b+0),(-height+offsetY)]);
-    }
-}
-for (let i = 7; i < 13; i++) {
-    const alphaRad = 2*Math.PI/steps*i;
-    console.log(alphaRad);
-    const a = Math.sqrt(2 - 2 * Math.cos(alphaRad))*radius*rotation;
-    const height = a/radius * Math.sqrt(Math.pow(radius,2) - Math.pow(a,2)/4);
-    
-    const b = Math.sqrt(Math.pow(a,2)-Math.pow(height,2));
-    if(steps / 2 > i){
-        coordinates.push([(b+offsetX),(height+offsetY)]);
-    }else{
-        coordinates.push([(b+offsetX),(-height+offsetY)]);
-    }
-}
-
 const keyframe = `@keyframes circle{${coordinates.map((v,i) => {
     return `${Math.floor(100/(coordinates.length-1)*i)}%{transform: translate(${v[0]}px, ${v[1]}px);}`;
 }).join("")}}`;
 
 console.log(keyframe);
 
+/*
+
+UNCOMMENT IF YOU WANT THIS KEYFRAME TO BE ADDED PROGRAMATICALLY INTO YOUR DOCUMENT AS A STYLESHEET
+
 styleSheet = document.createElement('style');
 styleSheet.type = 'text/css';
 document.head.appendChild(styleSheet);
 
 styleSheet.sheet.insertRule(keyframe);
+*/
