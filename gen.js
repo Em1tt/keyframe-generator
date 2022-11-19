56f75df5dc58fdca8aece102a85e5c8cde56e81e
Richard <55552821+Em1tt@users.noreply.github.com>
2022-11-19 01:44:12 +0100

    Update gen.js

--- gen.js
+++ gen.js
@@ -9,14 +9,14 @@ class CSSKeyframeGen {
         
         this.coordinates = [];
 
-        for (let i=0; i<steps+1; i++) { //+1 because we want it to return to original position;
-            const alphaRad = 2*Math.PI/steps*i;
-            const a = Math.sqrt(2 - 2 * Math.cos(alphaRad))*radius*rotation;
-            const height = a/radius * Math.sqrt(Math.pow(radius,2) - Math.pow(a,2)/4);
+        for (let i=0; i<this.steps+1; i++) { //+1 because we want it to return to original position;
+            const alphaRad = 2*Math.PI/this.steps*i;
+            const a = Math.sqrt(2 - 2 * Math.cos(alphaRad))*this.radius*this.rotation;
+            const height = a/this.radius * Math.sqrt(Math.pow(this.radius,2) - Math.pow(a,2)/4);
             const b = Math.sqrt(Math.pow(a,2)-Math.pow(height,2));
-            this.coordinates.push([(b+offsetX),(( steps/2 > i?1:-1) * height+offsetY)]); //Detect half of animation, invert Y value so the animation doesn't loop back the same way around
+            this.coordinates.push([(b+this.offsetX),(( this.steps/2 > i?1:-1) * height+this.offsetY)]); //Detect half of animation, invert Y value so the animation doesn't loop back the same way around
         };
-        this.kf = `@keyframes ${name}{${this.coordinates.map((v,i) => {
+        this.kf = `@keyframes ${this.name}{${this.coordinates.map((v,i) => {
             return `${Math.floor(100/(this.coordinates.length-1)*i)}%{transform: translate(${v[0]}px, ${v[1]}px);}`;
         }).join("")}}`;
     }
